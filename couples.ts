5b571be9dc10c6c341aba1c09bef3731ec3a6ddc
Danil Dynikov <ddynikov@gmail.com>
2024-02-05 22:34:26 +0200

    initial

---
+++ couples.ts
@@ -0,0 +1,24 @@
+const matrix: number[][] = [
+    [0, 0, 0, 0, 1, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 1, 0, 1],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+];
+
+const findCouplesInMatrix: number[][] = [];
+
+for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < 10; j++) {
+        if (matrix[i][j] === 1) {
+            findCouplesInMatrix.push([i + 1, j + 1]);
+        }
+    }
+}
+
+console.log(findCouplesInMatrix);
